51c55e80b03ff5518d0a3ffe48ce1199e8a8e8b1
Gerson David <gersonsoto5622@gmail.com>
2022-03-28 19:10:48 -0500

    [Added] Pokemon Static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,57 @@
 // next
+import { GetStaticProps } from 'next';
 import type { NextPage } from 'next';
 import { Button } from '@nextui-org/react';
 
 // Components
 import { Layout } from '../components/layout';
+import { pokeApi } from '../api';
 
-const Home: NextPage = () => {
+// Interfaces
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import Image from 'next/image';
+
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Listado de pokemons'>
-      <Button color='gradient'>Hola Mundo</Button>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.name}>
+          <p>{pokemon.name}</p>
+          <p>{pokemon.id}</p>
+          <Image
+            src={pokemon.img}
+            alt={pokemon.name}
+            width={100}
+            height={100}
+          />
+        </div>
+      ))}
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, id) => {
+    const pokeId: number = id++;
+    return {
+      name: pokemon.name,
+      url: pokemon.url,
+      id: pokeId,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`,
+    };
+  });
+
+  return {
+    props: {
+      pokemons,
+    },
+  };
 };
 
 export default Home;
